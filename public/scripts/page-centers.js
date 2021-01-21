bf8bc85b8c230991bdbe597b807936681636d7db
Jaciane Cruz <44266110+jacianeoc@users.noreply.github.com>
2021-01-21 01:30:13 -0300

    add leaflet

---
+++ public/scripts/page-centers.js
@@ -0,0 +1,23 @@
+//create map
+const map = L.map('mapid').setView([-7.226376,-35.8814862], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+}).addTo(map);
+
+const icon= L.icon({
+    iconUrl:'./public/images/marcacao1.svg',
+    iconSize: [68,68],
+    iconAnchor:[29,68],
+    popupAnchor: [150, -75]
+});
+
+//create popup overlay
+const popup= L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar dos gatinhos <a href="centers.html?1" class= "choose-center"> <img src="./public/images/Arrow 1.svg" > </a>')
+L.marker([-7.226376,-35.8814862], {icon}).addTo(map)
+    .bindPopup(popup);
